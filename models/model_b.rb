7bf164b3c830df1f5ca0b9a45cd5c9880d5e6ab2
Sulaiman Birashk <sully.birashk@gmail.com>
2021-11-12 17:56:15 -0700

    Add House Hash

---
+++ models/model_b.rb
@@ -0,0 +1,16 @@
+house = {
+  "color" => "blue",
+  "grass_type" => "turf",
+  "stories" => 3,
+  "kitchens" => 2,
+  "has_windows" => true,
+  "has_attic" => false,
+  "plants_in_garden" => ["Allium","Lavandula","Sedum","Leucanthemum","Baptisia"],
+  "colors_inside_the_house" => ["White","teal","black","brown"],
+  "house_specs" => {
+    "bedroom" => 4,
+    "bathrooms" => 3,
+    "kitchen_view" => false,
+    "full_showers" => true
+  }
+}
