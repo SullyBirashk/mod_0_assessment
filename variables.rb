d1eb80778d78c3160fb134bacff5af734aaff899
Sulaiman Birashk <sully.birashk@gmail.com>
2021-11-12 17:31:53 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,28 @@
+animal = "Shark"
+
+is_cuddly = false
+
+animal = "Horse"
+
+num_8 = 73
+
+num_3 = 80
+
+def quotient(a, b)
+  puts "Dividing #{a} / #{b}"
+  return a / b
+end
+
+puts quotient(num_3,num_8)
+
+
+favorite_films = ["Pacific Rim","Fate and Furious 8", "Tokoyo Drift"]
+
+
+favorite_films.shift
+
+
+puts favorite_films
+
+
+puts animal.length
