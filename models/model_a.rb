35f485f8667b27f65dec6b70786e847fa18690e7
Sulaiman Birashk <sully.birashk@gmail.com>
2021-11-12 17:43:13 -0700

    Add Laptop Hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop = {
+  "color" => "Silver",
+  "Brand" => "Apple",
+  "screen_size" => 13,
+  "battery_Life" => 20.2,
+  "M1_Chip" => true,
+  "Slow" => false,
+  "features" => ["Bluetooth", "Wifi", "Siri",],
+  "colors_avalable" => ["Silver", "White"]
+}
